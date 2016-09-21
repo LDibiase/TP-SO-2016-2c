34ae2319f471c56db65954e932d248dd349f1979
DiegoGar <diego.ignacio.garcia@hotmail.com>
2016-09-21 01:30:36 -0300

    Agrega constantes para utilizar fuse

--- PokeDexCliente/src/PokeDexCliente.h
+++ PokeDexCliente/src/PokeDexCliente.h
@@ -7,9 +7,20 @@
 
 #ifndef POKEDEXCLIENTE_H_
 #define POKEDEXCLIENTE_H_
-
+#define DEFAULT_FILE_PATH "/" DEFAULT_FILE_NAME
+#define DEFAULT_FILE_NAME "Osada"
+#define DEFAULT_FILE_CONTENT "Is Empty!\n"
 /* Definición de estructuras */
 
+static struct fuse_operations fuse_oper = {
+		.getattr = fuse_getattr,
+		.readdir = fuse_readdir,
+		.open = fuse_open,
+		.read = fuse_read,
+};
+
 /* Declaración de funciones */
+
+
 
 #endif /* POKEDEXCLIENTE_H_ */

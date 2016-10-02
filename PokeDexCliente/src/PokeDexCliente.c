61caef4b22bf62b92914f264b195c727815a7d6b
DiegoGar <diego.ignacio.garcia@hotmail.com>
2016-10-02 19:47:56 -0300

    Logra montar archivo fuse en carpeta tmp

--- PokeDexCliente/src/PokeDexCliente.c
+++ PokeDexCliente/src/PokeDexCliente.c
@@ -87,6 +87,18 @@ static int fuse_read(const char *path, char *buf, size_t size, off_t offset, str
 	return size;
 }
 
+static struct fuse_opt fuse_options[] = {
+		// Este es un parametro definido por nosotros
+		CUSTOM_FUSE_OPT_KEY("--welcome-msg %s", welcome_msg, 0),
+
+		// Estos son parametros por defecto que ya tiene FUSE
+		//FUSE_OPT_KEY("-V", KEY_VERSION),
+		//FUSE_OPT_KEY("--version", KEY_VERSION),
+		//FUSE_OPT_KEY("-h", KEY_HELP),
+		//FUSE_OPT_KEY("--help", KEY_HELP),
+		FUSE_OPT_END,
+};
+
 static struct fuse_operations fuse_oper = {
 		.getattr = fuse_getattr,
 		//.readdir = fuse_readdir,
@@ -94,19 +106,43 @@ static struct fuse_operations fuse_oper = {
 		.read = fuse_read,
 };
 
-int main(void) {
+int main(int argc, char *argv[]) {
 	struct socket* serv_socket_s;
+
+	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+
+		// Limpio la estructura que va a contener los parametros
+		memset(&runtime_options, 0, sizeof(struct t_runtime_options));
+
+		// Esta funcion de FUSE lee los parametros recibidos y los intepreta
+		if (fuse_opt_parse(&args, &runtime_options, fuse_options, NULL) == -1){
+			/** error parsing options */
+			perror("Invalid arguments!");
+			return EXIT_FAILURE;
+		}
+
+		// Si se paso el parametro --welcome-msg
+		// el campo welcome_msg deberia tener el
+		// valor pasado
+		if( runtime_options.welcome_msg != NULL ){
+			printf("%s\n", runtime_options.welcome_msg);
+		}
+
+		// Esta es la funcion principal de FUSE, es la que se encarga
+		// de realizar el montaje, comuniscarse con el kernel, delegar todo
+		// en varios threads
+		return fuse_main(args.argc, args.argv, &fuse_oper, NULL);
 
 	/* Creación del log */
 	logger = log_create(LOG_FILE_PATH, "ENTRENADOR", true, LOG_LEVEL_INFO);
 
 	serv_socket_s = conectarAServidor("127.0.0.1", "8080");
-	if(serv_socket_s->descriptor == 0)
-	{
-		log_info(logger, "Conexión fallida");
-		log_info(logger, serv_socket_s->error);
-		return EXIT_FAILURE;
-	}
+	//if(serv_socket_s->descriptor == 0)
+	//{
+		//log_info(logger, "Conexión fallida");
+		//log_info(logger, serv_socket_s->error);
+		//return EXIT_FAILURE;
+	//}
 
 	log_info(logger, "Conexión exitosa");
 

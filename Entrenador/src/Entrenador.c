6f9c3ec8c770a3bba38a349a71162087ceda6b83
TamiBerrocal <tamaraberrocal@gmail.com>
2016-10-04 21:30:57 -0300

    Corrección

--- Entrenador/src/Entrenador.c
+++ Entrenador/src/Entrenador.c
@@ -44,12 +44,12 @@ int main(void) {
 		ubicacionPokeNest.y = 0;
 
 		// Mientras que no se haya alcanzado la ubicación de la PokéNest a la que se desea llegar
-		//while(ubicacion.x != ubicacionPokeNest.x || ubicacion.y != ubicacionPokeNest.y) {
+		while(ubicacion.x != ubicacionPokeNest.x || ubicacion.y != ubicacionPokeNest.y) {
 			// Recibir mensaje TURNO
-			mensaje_t mensaje4;
+			mensaje_t mensajeTurno;
 
-			mensaje4.tipoMensaje = TURNO;
-			recibirMensaje(mapa_s, &mensaje4);
+			mensajeTurno.tipoMensaje = TURNO;
+			recibirMensaje(mapa_s, &mensajeTurno);
 			if(mapa_s->error != NULL)
 			{
 				log_info(logger, mapa_s->error);
@@ -59,7 +59,7 @@ int main(void) {
 				exit(error);
 			}
 
-			if(mensaje4.tipoMensaje == TURNO)
+			if(mensajeTurno.tipoMensaje == TURNO)
 			{
 				if(ubicacionPokeNest.x == 0 && ubicacionPokeNest.y == 0)
 				{
@@ -85,15 +85,15 @@ int main(void) {
 
 				// TODO Analizar otros posibles casos
 			}
-		//}
+		}
 
 		// Una vez alcanzada la ubicación de la PokéNest
 
 		// Recibir mensaje TURNO
-		//mensaje_t mensaje4;
+		mensaje_t mensajeTurno;
 
-		mensaje4.tipoMensaje = TURNO;
-		recibirMensaje(mapa_s, &mensaje4);
+		mensajeTurno.tipoMensaje = TURNO;
+		recibirMensaje(mapa_s, &mensajeTurno);
 		if(mapa_s->error != NULL)
 		{
 			log_info(logger, mapa_s->error);
@@ -103,7 +103,7 @@ int main(void) {
 			exit(error);
 		}
 
-		if(mensaje4.tipoMensaje == TURNO)
+		if(mensajeTurno.tipoMensaje == TURNO)
 		{
 			solicitarCaptura(mapa_s);
 			if(mapa_s->error != NULL)
@@ -133,10 +133,10 @@ int main(void) {
 
 			// Enviar mensaje OBJETIVOS_COMPLETADOS
 			paquete_t paquete;
-			mensaje_t mensaje11;
+			mensaje_t mensajeObjetivosCompletos;
 
-			mensaje11.tipoMensaje = OBJETIVOS_COMPLETADOS;
-			crearPaquete((void*) &mensaje11, &paquete);
+			mensajeObjetivosCompletos.tipoMensaje = OBJETIVOS_COMPLETADOS;
+			crearPaquete((void*) &mensajeObjetivosCompletos, &paquete);
 			if(paquete.tamanioPaquete == 0)
 			{
 				mapa_s->error = strdup("No se ha podido alocar memoria para el mensaje a enviarse");
@@ -255,14 +255,14 @@ socket_t* conectarAMapa(char* ip, char* puerto) {
 
 	// Enviar mensaje CONEXION_ENTRENADOR
 	paquete_t paquete;
-	mensaje1_t mensaje1;
+	mensaje1_t mensajeConexionEntrenador;
 
-	mensaje1.tipoMensaje = CONEXION_ENTRENADOR;
-	mensaje1.tamanioNombreEntrenador = strlen(configEntrenador.Nombre) + 1;
-	mensaje1.nombreEntrenador = configEntrenador.Nombre;
-	mensaje1.simboloEntrenador = *configEntrenador.Simbolo;
+	mensajeConexionEntrenador.tipoMensaje = CONEXION_ENTRENADOR;
+	mensajeConexionEntrenador.tamanioNombreEntrenador = strlen(configEntrenador.Nombre) + 1;
+	mensajeConexionEntrenador.nombreEntrenador = configEntrenador.Nombre;
+	mensajeConexionEntrenador.simboloEntrenador = *configEntrenador.Simbolo;
 
-	crearPaquete((void*) &mensaje1, &paquete);
+	crearPaquete((void*) &mensajeConexionEntrenador, &paquete);
 	if(paquete.tamanioPaquete == 0)
 	{
 		mapa_s->error = strdup("No se ha podido alocar memoria para el mensaje a enviarse");
@@ -282,10 +282,10 @@ socket_t* conectarAMapa(char* ip, char* puerto) {
 	free(paquete.paqueteSerializado);
 
 	// Recibir mensaje ACEPTA_CONEXION
-	mensaje_t mensaje;
+	mensaje_t mensajeAceptaConexion;
 
-	mensaje.tipoMensaje = ACEPTA_CONEXION;
-	recibirMensaje(mapa_s, &mensaje);
+	mensajeAceptaConexion.tipoMensaje = ACEPTA_CONEXION;
+	recibirMensaje(mapa_s, &mensajeAceptaConexion);
 	if(mapa_s->error != NULL)
 	{
 		log_info(logger, mapa_s->error);
@@ -293,7 +293,7 @@ socket_t* conectarAMapa(char* ip, char* puerto) {
 		return mapa_s;
 	}
 
-	switch(mensaje.tipoMensaje) {
+	switch(mensajeAceptaConexion.tipoMensaje) {
 	case RECHAZA_CONEXION:
 		mapa_s->error = string_from_format("Socket %d: su conexión ha sido rechazada", mapa_s->descriptor);
 		log_info(logger, mapa_s->error);
@@ -312,12 +312,12 @@ socket_t* conectarAMapa(char* ip, char* puerto) {
 void solicitarUbicacionPokeNest(socket_t* mapa_s, char idPokeNest, t_ubicacion* ubicacionPokeNest) {
 	// Enviar mensaje SOLICITA_UBICACION
 	paquete_t paquete;
-	mensaje5_t mensaje5;
+	mensaje5_t mensajeSolicitaUbicacion;
 
-	mensaje5.tipoMensaje = SOLICITA_UBICACION;
-	mensaje5.idPokeNest = idPokeNest;
+	mensajeSolicitaUbicacion.tipoMensaje = SOLICITA_UBICACION;
+	mensajeSolicitaUbicacion.idPokeNest = idPokeNest;
 
-	crearPaquete((void*) &mensaje5, &paquete);
+	crearPaquete((void*) &mensajeSolicitaUbicacion, &paquete);
 	if(paquete.tamanioPaquete == 0)
 	{
 		mapa_s->error = strdup("No se ha podido alocar memoria para el mensaje a enviarse");
@@ -337,10 +337,10 @@ void solicitarUbicacionPokeNest(socket_t* mapa_s, char idPokeNest, t_ubicacion* 
 	free(paquete.paqueteSerializado);
 
 	// Recibir mensaje BRINDA_UBICACION
-	mensaje6_t mensaje6;
+	mensaje6_t mensajeBrindaUbicacion;
 
-	mensaje6.tipoMensaje = BRINDA_UBICACION;
-	recibirMensaje(mapa_s, &mensaje6);
+	mensajeBrindaUbicacion.tipoMensaje = BRINDA_UBICACION;
+	recibirMensaje(mapa_s, &mensajeBrindaUbicacion);
 	if(mapa_s->error != NULL)
 	{
 		log_info(logger, mapa_s->error);
@@ -348,10 +348,10 @@ void solicitarUbicacionPokeNest(socket_t* mapa_s, char idPokeNest, t_ubicacion* 
 		return;
 	}
 
-	if(mensaje6.tipoMensaje == BRINDA_UBICACION)
+	if(mensajeBrindaUbicacion.tipoMensaje == BRINDA_UBICACION)
 	{
-		ubicacionPokeNest->x = mensaje6.ubicacionX;
-		ubicacionPokeNest->y = mensaje6.ubicacionY;
+		ubicacionPokeNest->x = mensajeBrindaUbicacion.ubicacionX;
+		ubicacionPokeNest->y = mensajeBrindaUbicacion.ubicacionY;
 	}
 
 	log_info(logger, "La ubicación de la PokéNest es (%d,%d)", ubicacionPokeNest->x, ubicacionPokeNest->y);
@@ -389,12 +389,12 @@ void solicitarDesplazamiento(socket_t* mapa_s, t_ubicacion* ubicacion, t_ubicaci
 
 	// Enviar mensaje SOLICITA_DESPLAZAMIENTO
 	paquete_t paquete;
-	mensaje7_t mensaje7;
+	mensaje7_t mensajeSolicitaDesplazamiento;
 
-	mensaje7.tipoMensaje = SOLICITA_DESPLAZAMIENTO;
-	mensaje7.direccion = movimiento;
+	mensajeSolicitaDesplazamiento.tipoMensaje = SOLICITA_DESPLAZAMIENTO;
+	mensajeSolicitaDesplazamiento.direccion = movimiento;
 
-	crearPaquete((void*) &mensaje7, &paquete);
+	crearPaquete((void*) &mensajeSolicitaDesplazamiento, &paquete);
 	if(paquete.tamanioPaquete == 0)
 	{
 		mapa_s->error = strdup("No se ha podido alocar memoria para el mensaje a enviarse");
@@ -414,10 +414,10 @@ void solicitarDesplazamiento(socket_t* mapa_s, t_ubicacion* ubicacion, t_ubicaci
 	free(paquete.paqueteSerializado);
 
 	// Recibir mensaje CONFIRMA_DESPLAZAMIENTO
-	mensaje8_t mensaje8;
+	mensaje8_t mensajeConfirmaDesplazamiento;
 
-	mensaje8.tipoMensaje = CONFIRMA_DESPLAZAMIENTO;
-	recibirMensaje(mapa_s, &mensaje8);
+	mensajeConfirmaDesplazamiento.tipoMensaje = CONFIRMA_DESPLAZAMIENTO;
+	recibirMensaje(mapa_s, &mensajeConfirmaDesplazamiento);
 	if(mapa_s->error != NULL)
 	{
 		log_info(logger, mapa_s->error);
@@ -425,10 +425,10 @@ void solicitarDesplazamiento(socket_t* mapa_s, t_ubicacion* ubicacion, t_ubicaci
 		return;
 	}
 
-	if(mensaje8.tipoMensaje == CONFIRMA_DESPLAZAMIENTO)
+	if(mensajeConfirmaDesplazamiento.tipoMensaje == CONFIRMA_DESPLAZAMIENTO)
 	{
-		ubicacion->x = mensaje8.ubicacionX;
-		ubicacion->y = mensaje8.ubicacionY;
+		ubicacion->x = mensajeConfirmaDesplazamiento.ubicacionX;
+		ubicacion->y = mensajeConfirmaDesplazamiento.ubicacionY;
 	}
 
 	log_info(logger, "La ubicación actual del entrenador es (%d,%d)", ubicacion->x, ubicacion->y);
@@ -439,10 +439,10 @@ void solicitarDesplazamiento(socket_t* mapa_s, t_ubicacion* ubicacion, t_ubicaci
  void solicitarCaptura(socket_t* mapa_s) {
 	// Enviar mensaje SOLICITA_CAPTURA
 	paquete_t paquete;
-	mensaje_t mensaje9;
+	mensaje_t mensajeSolicitaCaptura;
 
-	mensaje9.tipoMensaje = SOLICITA_CAPTURA;
-	crearPaquete((void*) &mensaje9, &paquete);
+	mensajeSolicitaCaptura.tipoMensaje = SOLICITA_CAPTURA;
+	crearPaquete((void*) &mensajeSolicitaCaptura, &paquete);
 	if(paquete.tamanioPaquete == 0)
 	{
 		mapa_s->error = strdup("No se ha podido alocar memoria para el mensaje a enviarse");
@@ -462,10 +462,10 @@ void solicitarDesplazamiento(socket_t* mapa_s, t_ubicacion* ubicacion, t_ubicaci
 	free(paquete.paqueteSerializado);
 
 	// Recibir mensaje CONFIRMA_CAPTURA
-	mensaje_t mensaje10;
+	mensaje_t mensajeConfirmaCaptura;
 
-	mensaje10.tipoMensaje = CONFIRMA_CAPTURA;
-	recibirMensaje(mapa_s, &mensaje10);
+	mensajeConfirmaCaptura.tipoMensaje = CONFIRMA_CAPTURA;
+	recibirMensaje(mapa_s, &mensajeConfirmaCaptura);
 	if(mapa_s->error != NULL)
 	{
 		log_info(logger, mapa_s->error);
@@ -473,7 +473,7 @@ void solicitarDesplazamiento(socket_t* mapa_s, t_ubicacion* ubicacion, t_ubicaci
 		return;
 	}
 
-	if(mensaje10.tipoMensaje == CONFIRMA_CAPTURA)
+	if(mensajeConfirmaCaptura.tipoMensaje == CONFIRMA_CAPTURA)
 	{
 		// TODO Agregar lógica para obtener metadata del pokémon que hemos atrapado (determinar si el mapa nos la provee o si la debemos recuperar del FS)
 	}

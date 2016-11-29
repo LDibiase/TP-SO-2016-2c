dedb761718154325aca6964f0dfee6a32774158a
LDibiase <lucas.dibiase@gmail.com>
2016-11-28 22:02:51 -0300

    Fix del merge choto que hizo git

--- Mapa/src/Mapa.c
+++ Mapa/src/Mapa.c
@@ -658,13 +658,12 @@ t_list* cargarPokenests() {
 int obtenerCantidadRecursos(char* nombrePokemon, char* rutaPokenest, t_list* metadatasPokemones) {
 	int cantidad = 0;
 	t_config* config;
-
 	char* nombreArchivoPokemonAux = strdup(rutaPokenest);
 	string_append(&nombreArchivoPokemonAux, "/");
 	string_append(&nombreArchivoPokemonAux, nombrePokemon);
-	string_append(&nombreArchivoPokemonAux, "/");
 
 	char* numeroPokemon = strdup("001");
+
 	char* nombreArchivoPokemon = strdup(nombreArchivoPokemonAux);
 	string_append(&nombreArchivoPokemon, numeroPokemon);
 	string_append(&nombreArchivoPokemon, ".dat");
